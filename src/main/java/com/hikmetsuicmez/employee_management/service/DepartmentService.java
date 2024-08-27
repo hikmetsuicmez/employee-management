ff1dd3e565efc780b5cfe1bca62b392fc758c624
Hikmet <hikmet.suicmez1@gmail.com>
2024-08-27 20:56:49 +0300

    Added lombok @Slf4j logging to all service layers.

--- src/main/java/com/hikmetsuicmez/employee_management/service/DepartmentService.java
+++ src/main/java/com/hikmetsuicmez/employee_management/service/DepartmentService.java
@@ -4,12 +4,14 @@ import com.hikmetsuicmez.employee_management.dto.DepartmentDTO;
 import com.hikmetsuicmez.employee_management.entity.Department;
 import com.hikmetsuicmez.employee_management.mapper.DepartmentMapper;
 import com.hikmetsuicmez.employee_management.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
@@ -21,16 +23,19 @@ public class DepartmentService {
     }
 
     public List<DepartmentDTO> getAllDepartments() {
+        log.info("getAllDepartments");
         List<Department> departments = departmentRepository.findAll();
         return departments.stream().map(departmentMapper::toDepartmentDTO)
                 .toList();
     }
 
     public Optional<DepartmentDTO> getDepartmentById(Long id) {
+        log.debug("Attempting to retrieve department with id: {}", id);
         return departmentRepository.findById(id).map(departmentMapper::toDepartmentDTO);
     }
 
     public DepartmentDTO createDepartment(DepartmentDTO departmentDTO) {
+        log.debug("Attempting to create department {}", departmentDTO);
         Department department = departmentMapper.toDepartment(departmentDTO);
         departmentRepository.save(department);
         return departmentMapper.toDepartmentDTO(department);
@@ -38,6 +43,7 @@ public class DepartmentService {
     }
 
     public void deleteDepartment(Long id) {
+        log.debug("Attempting to delete department with id: {}", id);
         departmentRepository.deleteById(id);
     }
 
--- src/main/java/com/hikmetsuicmez/employee_management/service/EmployeeService.java
+++ src/main/java/com/hikmetsuicmez/employee_management/service/EmployeeService.java
@@ -4,12 +4,14 @@ import com.hikmetsuicmez.employee_management.dto.EmployeeDTO;
 import com.hikmetsuicmez.employee_management.entity.Employee;
 import com.hikmetsuicmez.employee_management.mapper.EmployeeMapper;
 import com.hikmetsuicmez.employee_management.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
@@ -21,21 +23,26 @@ public class EmployeeService {
     }
 
     public List<EmployeeDTO> getAllEmployees() {
+        log.info("getAllEmployees");
         List<Employee> employees = employeeRepository.findAll();
         return employees.stream().map(employeeMapper::toEmployeeDTO).toList();
     }
 
     public Optional<EmployeeDTO> getEmployeeById(Long id) {
+        log.info("getEmployeeById");
+        log.debug("Attempting to retrieve employee with id: {}", id);
         return employeeRepository.findById(id).map(employeeMapper::toEmployeeDTO);
     }
 
     public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
+        log.info("saveEmployee");
         Employee employee = employeeMapper.toEmployee(employeeDTO);
         employeeRepository.save(employee);
         return employeeMapper.toEmployeeDTO(employee);
     }
 
     public void deleteEmployee(Long id) {
+        log.info("deleteEmployee");
         employeeRepository.deleteById(id);
     }
 

098e578c14fc67ff72ebd38c10dfd85bf850e7dd
Hikmet <hikmet.suicmez1@gmail.com>
2024-08-27 10:46:19 +0300

    Added Employee and Department service layers

---
+++ src/main/java/com/hikmetsuicmez/employee_management/service/DepartmentService.java
@@ -0,0 +1,33 @@
+package com.hikmetsuicmez.employee_management.service;
+
+import com.hikmetsuicmez.employee_management.entity.Department;
+import com.hikmetsuicmez.employee_management.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    public Optional<Department> getDepartmentById(Long id) {
+        return departmentRepository.findById(id);
+    }
+
+    public Department createDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public void deleteDepartment(Long id) {
+        departmentRepository.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/hikmetsuicmez/employee_management/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.hikmetsuicmez.employee_management.service;
+
+import com.hikmetsuicmez.employee_management.entity.Employee;
+import com.hikmetsuicmez.employee_management.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+}
